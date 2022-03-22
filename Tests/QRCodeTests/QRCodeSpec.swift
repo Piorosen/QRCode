bcddcbc3d948c91ece9925b832152bbe90b055c9
Piorosen <aoikazto@naver.com>
2022-03-22 11:53:22 +0900

    냐항?

---
+++ Tests/QRCodeTests/QRCodeSpec.swift
@@ -0,0 +1,255 @@
+import Quick
+import Nimble
+
+@testable import QRCode
+
+class QRCodeSpec: QuickSpec {
+    override func spec() {
+        describe("QRCode") {
+            var qrCode: QRCode!
+            let size = CGSize(width: 300, height: 300)
+            let inherentSize = CGSize(width: 23.0, height: 23.0)
+
+            describe("init(data:color:backgroundColor:size:inputCorrection:)") {
+                var data: Data!
+
+                beforeEach {
+                    data = "awesome".data(using: .isoLatin1)
+                }
+
+                it("creates a valid instance") {
+                    qrCode = QRCode(data: data)
+                    expect(qrCode).toNot(beNil())
+                }
+
+                it("encapsulates the passed data as is") {
+                    qrCode = QRCode(data: data)
+                    expect(qrCode.data).to(equal(data))
+                }
+
+                it("can have optional params") {
+                    qrCode = QRCode(data: data, color: UIColor.red, size: size)
+                    expect(qrCode.color).to(equal(UIColor.red))
+                    expect(qrCode.size).to(equal(size))
+                }
+            }
+
+            describe("init(string:color:backgroundColor:size:inputCorrection:)") {
+                var string: String!
+
+                beforeEach {
+                    string = "awesome"
+                }
+
+                it("creates a valid instance") {
+                    qrCode = QRCode(string: string)
+                    expect(qrCode).toNot(beNil())
+                }
+
+                it("encapsulates the passed string as ISO Latin1 converted data") {
+                    qrCode = QRCode(string: string)
+                    expect(qrCode.data).to(equal(string.data(using: .isoLatin1)))
+                }
+
+                it("can have optional params") {
+                    qrCode = QRCode(string: string, backgroundColor: UIColor.blue, size: size)
+                    expect(qrCode.backgroundColor).to(equal(UIColor.blue))
+                    expect(qrCode.size).to(equal(size))
+                }
+            }
+
+            describe("init(url:color:backgroundColor:size:inputCorrection:)") {
+                var url: URL!
+
+                beforeEach {
+                    url = URL(string: "http://example.com/amazing")
+                }
+
+                it("creates a valid instance") {
+                    qrCode = QRCode(url: url)
+                    expect(qrCode).toNot(beNil())
+                }
+
+                it("encapsulates the passed url as ISO Latin1 converted string data") {
+                    qrCode = QRCode(url: url)
+                    expect(qrCode.data).to(equal(url.absoluteString.data(using: .isoLatin1)))
+                }
+
+                it("can have optional params") {
+                    qrCode = QRCode(url: url, size: size)
+                    expect(qrCode.size).to(equal(size))
+                }
+            }
+
+            describe("image()") {
+                context("with default or sufficient size") {
+                    it("creates image of optimal size for screen (1pt / datapoint)") {
+                        expect(try? QRCode(string: "hallo")!.image().scale).to(equal(UIScreen.main.scale))
+                        expect(try? QRCode(string: "hallo")!.image().size).to(equal(inherentSize))
+                    }
+                }
+
+                it("throws when width is too small for amount of data") {
+                    let desiredSize = CGSize(width: 10.0, height: 23.0)
+                    expect {
+                        try QRCode(string: "hallo", size: desiredSize)?.image()
+                    }.to(throwError(QRCode.GenerationError.desiredSizeTooSmall(desired: desiredSize, actual: inherentSize)))
+                }
+
+                it("throws when height is too small for amount of data") {
+                    let desiredSize = CGSize(width: 23.0, height: 10.0)
+                    expect {
+                        try QRCode(string: "hallo", size: desiredSize)?.image()
+                    }.to(throwError(QRCode.GenerationError.desiredSizeTooSmall(desired: desiredSize, actual: inherentSize)))
+                }
+
+                it("throws when data ins generally too large") {
+                    expect {
+                        try QRCode(string: tooLongString())?.image()
+                    }.to(throwError(QRCode.GenerationError.inputDataTooLarge(size: 2954)))
+                }
+            }
+
+            describe("unsafeImage") {
+                context("with empty data") {
+                    it("still returns proper image") {
+                        expect(QRCode(data: Data()).unsafeImage).toNot(beNil())
+                    }
+                }
+
+                context("with non-empty data") {
+                    beforeEach {
+                        qrCode = QRCode(string: "hallo")
+                    }
+
+                    it("creates image of optimal size for screen (1pt / datapoint)") {
+                        expect(qrCode.unsafeImage!.scale).to(equal(UIScreen.main.scale))
+                        expect(qrCode.unsafeImage!.size).to(equal(inherentSize))
+                    }
+
+                    context("scale") {
+                        it("properly adjusts UIImage scale") {
+                            expect(QRCode(string: "hallo", scale: 1.0)!.unsafeImage!.scale).to(equal(1.0))
+                            expect(QRCode(string: "hallo", scale: 2.0)!.unsafeImage!.scale).to(equal(2.0))
+                            expect(QRCode(string: "hallo", scale: 3.0)!.unsafeImage!.scale).to(equal(3.0))
+                        }
+
+                        it("properly maintains UIImage target size") {
+                            expect(QRCode(string: "hallo", scale: 1.0)!.unsafeImage!.size).to(equal(inherentSize))
+                            expect(QRCode(string: "hallo", scale: 2.0)!.unsafeImage!.size).to(equal(inherentSize))
+                            expect(QRCode(string: "hallo", scale: 3.0)!.unsafeImage!.size).to(equal(inherentSize))
+                        }
+                    }
+
+                    it("can resize the image to desired dimensions, respecting scale") {
+                        qrCode.size = size
+                        qrCode.scale = 3
+                        expect(qrCode.unsafeImage!.size).to(equal(size))
+                    }
+
+                    it("returns nil when width is too small for amount of data") {
+                        let desiredSize = CGSize(width: 10.0, height: 23.0)
+                        expect(QRCode(string: "hallo", size: desiredSize)?.unsafeImage).to(beNil())
+                    }
+
+                    it("returns nil when height is too small for amount of data") {
+                        let desiredSize = CGSize(width: 23.0, height: 10.0)
+                        expect(QRCode(string: "hallo", size: desiredSize)?.unsafeImage).to(beNil())
+                    }
+
+                    it("returns nil when data ins generally too large") {
+                        expect(QRCode(string: tooLongString())?.unsafeImage).to(beNil())
+                    }
+                }
+            }
+
+            describe("Equatable") {
+                it("is equal when all attributes are equal") {
+                    expect(QRCode(string: "hallo")).to(equal(QRCode(data: "hallo".data(using: .isoLatin1)!)))
+                }
+
+                it("is not equal when any one attribute is not equal") {
+                    expect(QRCode(string: "hallo")).toNot(equal(QRCode(string: "hallo1")))
+                    expect(QRCode(string: "hallo", color: UIColor.red)).toNot(equal(QRCode(string: "hallo", color: UIColor.blue)))
+                    expect(QRCode(string: "hallo", backgroundColor: UIColor.green)).toNot(equal(QRCode(string: "hallo", backgroundColor: UIColor.brown)))
+                    expect(QRCode(string: "hallo", size: CGSize(width: 100, height: 100))).toNot(equal(QRCode(string: "hallo", size: CGSize(width: 100, height: 200))))
+                    expect(QRCode(string: "hallo", scale: 1)).toNot(equal(QRCode(string: "hallo", scale: 2)))
+                    expect(QRCode(string: "hallo", inputCorrection: .low)).toNot(equal(QRCode(string: "hallo", inputCorrection: .high)))
+                }
+            }
+
+            describe("caching") {
+                it("returns a cached image on subsequent calls") {
+                    let qrCode = QRCode(string: "hallo")!
+                    expect(qrCode.unsafeImage).to(equal(qrCode.unsafeImage))
+                }
+
+                it("flushes the cache once data changes") {
+                    var qrCode = QRCode(string: "hallo")!
+                    let firstImage = qrCode.unsafeImage
+                    qrCode.data = "hallo".data(using: .isoLatin1)!
+                    expect(firstImage).to(equal(qrCode.unsafeImage))
+                    qrCode.data = "different".data(using: .isoLatin1)!
+
+                    try? expect(firstImage).toNot(equal(qrCode.image()))
+                }
+
+                it("flushes the cache once color changes") {
+                    var qrCode = QRCode(string: "hallo")!
+                    let firstImage = qrCode.unsafeImage
+                    qrCode.color = UIColor.black
+                    expect(firstImage).to(equal(qrCode.unsafeImage))
+                    qrCode.color = UIColor.red
+
+                    try? expect(firstImage).toNot(equal(qrCode.image()))
+                }
+
+                it("flushes the cache once backgroundColor changes") {
+                    var qrCode = QRCode(string: "hallo")!
+                    let firstImage = qrCode.unsafeImage
+                    qrCode.backgroundColor = UIColor.white
+                    expect(firstImage).to(equal(qrCode.unsafeImage))
+                    qrCode.backgroundColor = UIColor.red
+
+                    try? expect(firstImage).toNot(equal(qrCode.image()))
+                }
+
+                it("flushes the cache once size changes") {
+                    var qrCode = QRCode(string: "hallo")!
+                    let firstImage = qrCode.unsafeImage
+                    qrCode.size = nil
+                    expect(firstImage).to(equal(qrCode.unsafeImage))
+                    qrCode.size = CGSize(width: 100, height: 100)
+
+                    try? expect(firstImage).toNot(equal(qrCode.image()))
+                }
+
+                it("flushes the cache once scale changes") {
+                    var qrCode = QRCode(string: "hallo")!
+                    let firstImage = qrCode.unsafeImage
+                    qrCode.scale = UIScreen.main.scale
+                    expect(firstImage).to(equal(qrCode.unsafeImage))
+                    qrCode.scale = 42
+
+                    try? expect(firstImage).toNot(equal(qrCode.image()))
+                }
+
+                it("flushes the cache once inputCorrection changes") {
+                    var qrCode = QRCode(string: "hallo")!
+                    let firstImage = qrCode.unsafeImage
+                    qrCode.inputCorrection = .low
+                    expect(firstImage).to(equal(qrCode.unsafeImage))
+                    qrCode.inputCorrection = .medium
+
+                    try? expect(firstImage).toNot(equal(qrCode.image()))
+                }
+            }
+        }
+    }
+}
+
+func tooLongString() -> String {
+    let maximumQRCodeByteCount = 2953
+    let tooLongString = (0...maximumQRCodeByteCount).map { _ in "x" }.reduce("", { $0 + $1 })
+    return tooLongString
+}
---
+++ Tests/QRCodeTests/QRCodeViewSpec.swift
@@ -0,0 +1,311 @@
+import Quick
+import Nimble
+import UIKit
+
+@testable import QRCode
+
+class QRCodeViewSpec: QuickSpec {
+    override func spec() {
+        describe("QRCodeView") {
+            var viewController: UIViewController!
+            var imageGenerator: FakeQRCodeImageGenerator!
+            var qrCode: QRCode!
+            var qrCodeView: QRCodeView!
+            let screenSize = CGSize(width: 500, height: 800)
+            var widthConstraint: NSLayoutConstraint!
+            var heightConstraint: NSLayoutConstraint!
+
+            beforeEach {
+                imageGenerator = FakeQRCodeImageGenerator()
+                qrCode = QRCode(imageGenerator: imageGenerator,
+                                data: "hello".data(using: .isoLatin1)!,
+                                size: CGSize(width: 100, height: 100))
+                qrCodeView = QRCodeView(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: 200, height: 200)))
+                qrCodeView.translatesAutoresizingMaskIntoConstraints = false
+
+                viewController = UIViewController()
+                viewController.view.addSubview(qrCodeView)
+                viewController.view.translatesAutoresizingMaskIntoConstraints = false
+
+                widthConstraint = NSLayoutConstraint(item: viewController.view!, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: screenSize.width)
+                heightConstraint = NSLayoutConstraint(item: viewController.view!, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: screenSize.height)
+                viewController.view.addConstraint(widthConstraint)
+                viewController.view.addConstraint(heightConstraint)
+
+                stretch(childView: qrCodeView, toFullyExpandInsideParentView: viewController.view)
+
+                waitUntil { done in
+                    qrCodeView.qrCode = qrCode
+                    qrCodeView.contentMode = .scaleAspectFit
+                    DispatchQueue.main.async(execute: viewController.view.layoutIfNeeded)
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1, execute: done)
+                }
+            }
+
+            it("requires constraint based layout") {
+                expect(QRCodeView.requiresConstraintBasedLayout).to(beTruthy())
+            }
+
+            it("resizes to the right dimensions") {
+                expect(qrCodeView.bounds.size).toEventually(equal(screenSize))
+            }
+
+            it("updates the image in it's child UIImageView") {
+                let idealFitSize = CGSize(width: screenSize.width, height: screenSize.width)
+                expect(imageView(inside: qrCodeView).image?.size).toEventually(equal(idealFitSize))
+            }
+
+            it("sizes the QRCode to the ideal fit for the new frame") {
+                let idealFitSize = CGSize(width: screenSize.width, height: screenSize.width)
+                expect(qrCodeView.qrCode?.size).toEventually(equal(idealFitSize))
+            }
+
+            describe("changing the view's size") {
+                let updatedSize = CGSize(width: 150, height: 200)
+
+                beforeEach {
+                    waitUntil { done in
+                        widthConstraint.constant = updatedSize.width
+                        heightConstraint.constant = updatedSize.height
+                        viewController.view.setNeedsLayout()
+                        DispatchQueue.main.async(execute: viewController.view.layoutIfNeeded)
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1, execute: done)
+                    }
+                }
+
+                it("updates the QRCode size to the ideal fit for the new frame") {
+                    let idealFitSize = CGSize(width: updatedSize.width, height: updatedSize.width)
+                    expect(qrCodeView.qrCode?.size).toEventually(equal(idealFitSize))
+                }
+
+                it("sets the newly generated, ideally sized image") {
+                    let idealFitSize = CGSize(width: updatedSize.width, height: updatedSize.width)
+                    expect(imageView(inside: qrCodeView).image?.size).toEventually(equal(idealFitSize))
+                }
+            }
+
+            describe("modifying the view hierarchy") {
+                let padding: CGFloat = 100.0
+
+                beforeEach {
+                    waitUntil { done in
+                        let newView = UIView(frame: CGRect.zero)
+                        newView.translatesAutoresizingMaskIntoConstraints = false
+
+                        viewController.view.addSubview(newView)
+                        qrCodeView.removeFromSuperview()
+                        newView.addSubview(qrCodeView)
+
+                        viewController.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-\(padding)-[new]-\(padding)-|", options: [], metrics: nil, views: ["new": newView]))
+                        viewController.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-\(padding)-[new]-\(padding)-|", options: [], metrics: nil, views: ["new": newView]))
+                        stretch(childView: qrCodeView, toFullyExpandInsideParentView: newView)
+
+                        DispatchQueue.main.async(execute: viewController.view.layoutIfNeeded)
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1, execute: done)
+                    }
+                }
+
+                it("handles being added to different superview correctly") {
+                    let idealFitSize = CGSize(width: screenSize.width - 2 * padding, height: screenSize.width - 2 * padding)
+                    expect(qrCodeView.qrCode?.size).toEventually(equal(idealFitSize))
+                }
+            }
+
+            describe("reacting to contentMode changes") {
+                context(".scaleAspectFit") {
+                    beforeEach {
+                        waitUntil { done in
+                            qrCodeView.contentMode = .scaleAspectFit
+                            DispatchQueue.main.async(execute: qrCodeView.layoutIfNeeded)
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1, execute: done)
+                        }
+                    }
+                    let idealFitSize = CGSize(width: screenSize.width, height: screenSize.width)
+
+                    it("updates the image in it's child UIImageView") {
+                        expect(imageView(inside: qrCodeView).image?.size).toEventually(equal(idealFitSize))
+                    }
+
+                    it("sizes the QRCode to the ideal fit for the new frame") {
+                        expect(qrCodeView.qrCode?.size).toEventually(equal(idealFitSize))
+                    }
+                }
+
+                context(".scaleAspectFill") {
+                    beforeEach {
+                        waitUntil { done in
+                            qrCodeView.contentMode = .scaleAspectFill
+                            qrCodeView.sizingBehavior = .alwaysRender
+                            DispatchQueue.main.async(execute: qrCodeView.layoutIfNeeded)
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1, execute: done)
+                        }
+                    }
+                    let idealFitSize = CGSize(width: screenSize.height, height: screenSize.height)
+
+                    it("updates the image in it's child UIImageView") {
+                        expect(imageView(inside: qrCodeView).image?.size).toEventually(equal(idealFitSize))
+                    }
+
+                    it("sizes the QRCode to the ideal fit for the new frame") {
+                        expect(qrCodeView.qrCode?.size).toEventually(equal(idealFitSize))
+                    }
+                }
+
+                context(".scaleToFill") {
+                    beforeEach {
+                        waitUntil { done in
+                            qrCodeView.contentMode = .scaleToFill
+                            DispatchQueue.main.async(execute: qrCodeView.layoutIfNeeded)
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1, execute: done)
+                        }
+                    }
+                    let idealFitSize = screenSize
+
+                    it("updates the image in it's child UIImageView") {
+                        expect(imageView(inside: qrCodeView).image?.size).toEventually(equal(idealFitSize))
+                    }
+
+                    it("sizes the QRCode to the ideal fit for the new frame") {
+                        expect(qrCodeView.qrCode?.size).toEventually(equal(idealFitSize))
+                    }
+                }
+
+                context("default") {
+                    beforeEach {
+                        waitUntil { done in
+                            qrCodeView.contentMode = .center
+                            DispatchQueue.main.async(execute: qrCodeView.layoutIfNeeded)
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1, execute: done)
+                        }
+                    }
+                    let idealFitSize = FakeQRCodeImageGenerator.intrinsicImageSize
+
+                    it("updates the image in it's child UIImageView") {
+                        expect(imageView(inside: qrCodeView).image?.size).toEventually(equal(idealFitSize))
+                    }
+
+                    it("unsets the QR code desired size (to nil)") {
+                        expect(qrCodeView.qrCode?.size).toEventually(beNil())
+                    }
+                }
+            }
+
+            describe("intrinsicContentSize") {
+                context("with a set QRCode") {
+                    it("returns the actual size of the generated image") {
+                        expect(qrCodeView.intrinsicContentSize).to(equal(qrCode.unsafeImage?.size))
+                    }
+                }
+
+                context("without a set QRCode") {
+                    beforeEach {
+                        qrCodeView.qrCode = nil
+                    }
+
+                    it("returns invalid size (size based on UIiewNoInstrinsicMetric)") {
+                        let noIntrinsicSize = CGSize(width: UIView.noIntrinsicMetric, height: UIView.noIntrinsicMetric)
+                        expect(qrCodeView.intrinsicContentSize).to(equal(noIntrinsicSize))
+                    }
+                }
+            }
+
+            describe("sizing behavior") {
+                let tooSmallSize = CGSize(width: 10, height: 10)
+
+                beforeEach {
+                    qrCodeView.qrCode = QRCode(imageGenerator: DefaultQRCodeImageGenerator(),
+                                               data: "hello".data(using: .isoLatin1)!)
+                    waitUntil { done in
+                        widthConstraint.constant = tooSmallSize.width
+                        heightConstraint.constant = tooSmallSize.height
+                        viewController.view.setNeedsLayout()
+                        DispatchQueue.main.async(execute: viewController.view.layoutIfNeeded)
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2, execute: done)
+                    }
+                }
+
+                context(".alwaysRender") {
+                    beforeEach {
+                        waitUntil { done in
+                            qrCodeView.sizingBehavior = .alwaysRender
+                            DispatchQueue.main.async(execute: qrCodeView.layoutIfNeeded)
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2, execute: done)
+                        }
+                    }
+
+                    it("renders the image even when target size is too small") {
+                        expect(imageView(inside: qrCodeView).image).toEventuallyNot(beNil())
+                    }
+                }
+
+                context(".hideWhenToSmall") {
+                    context("when size is generally too small") {
+                        beforeEach {
+                            waitUntil { done in
+                                qrCodeView.sizingBehavior = .hideWhenTooSmall
+                                DispatchQueue.main.async(execute: qrCodeView.layoutIfNeeded)
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.2, execute: done)
+                            }
+                        }
+
+                        it("does NOT render the image") {
+                            expect(imageView(inside: qrCodeView).image).toEventually(beNil())
+                        }
+                    }
+
+                    context("when .scaleAspectFill is set with a non-square view") {
+                        beforeEach {
+                            widthConstraint.constant = 120
+                            heightConstraint.constant = 130
+                            viewController.view.setNeedsLayout()
+
+                            waitUntil { done in
+                                qrCodeView.contentMode = .scaleAspectFill
+                                qrCodeView.sizingBehavior = .hideWhenTooSmall
+                                DispatchQueue.main.async(execute: qrCodeView.layoutIfNeeded)
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.2, execute: done)
+                            }
+                        }
+
+                        it("does not render the image") {
+                            expect(imageView(inside: qrCodeView).image).toEventually(beNil())
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+// MARK: - Spec Helpers
+
+func stretch(childView: UIView, toFullyExpandInsideParentView parentView: UIView) {
+    parentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[child]|", options: [], metrics: nil, views: ["child": childView]))
+    parentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[child]|", options: [], metrics: nil, views: ["child": childView]))
+}
+
+func imageView(inside view: QRCodeView) -> UIImageView {
+    return view.subviews.first as! UIImageView
+}
+
+// MARK: - Fakes
+
+class FakeQRCodeImageGenerator: QRCodeImageGenerator {
+    static let intrinsicImageSize = CGSize(width: 42, height: 42)
+    var lastCode: QRCode?
+    var lastImage: UIImage?
+
+    func image(for code: QRCode) throws -> UIImage {
+        lastCode = code
+        lastImage = generateImage(for: code)
+        return lastImage!
+    }
+
+    private func generateImage(for code: QRCode) -> UIImage {
+        let newSize = code.size ?? FakeQRCodeImageGenerator.intrinsicImageSize
+        UIGraphicsBeginImageContext(newSize)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image!
+    }
+}
